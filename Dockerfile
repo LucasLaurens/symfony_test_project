9f047ae325b473ef61b5bbf46a1dbc56a31bedf0
LucasLaurens <lucaslaurens.74@gmail.com>
2022-01-26 08:55:08 +0100

    Last enhance to get the messenger

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM php:7-jessie
+
+RUN apt-get update \
+    && apt-get install -y \
+        librabbitmq-dev \
+        libssh-dev \
+    && docker-php-ext-install \
+        bcmath \
+        sockets \
+    && pecl install amqp \
+    && docker-php-ext-enable amqp
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Mailer\MailerInterface;
 
 class TestController extends AbstractController
 {
@@ -39,11 +38,7 @@ class TestController extends AbstractController
             $em->persist($task);
             $em->flush();
 
-            try {
-                $bus->dispatch(new MailNotification($task->getId(), $task->getSubject(), $task->getContent()));
-            } catch (\Exception $e) {
-                throw new \Exception($e->getMessage());
-            }
+            $bus->dispatch(new MailNotification($task->getId(), $task->getSubject(), $task->getContent()));
  
             return $this->redirectToRoute('test');
         }
